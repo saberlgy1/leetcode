4ec9d20eb937a9ead68db2b178f333889007be1a
刘光远 <liuguangyuan@neusoft.com>
2020-07-10 14:34:00 +0800

    - 0709

--- src/com/cute/leetcode/editor/cn/TrappingRainWater.java
+++ src/com/cute/leetcode/editor/cn/TrappingRainWater.java
@@ -62,8 +62,6 @@ public class TrappingRainWater {
 
 
             //按列求和 暴力法
-            int left = 0;
-            int right = 0;
             int sum = 0;
             int len = height.length;
 //            for (int i = 1; i <= len - 2; i++) {
@@ -81,40 +79,116 @@ public class TrappingRainWater {
 //                left = 0;
 //                right = 0;
 //            }
+
             //优化1 取数组最大值、左右指针少获取一次
             //获取最大值所在索引
-            int index = getMaxIndex(height);
+//            int index = getMaxIndex(height);
+//
+//
+//            for (int i = 1; i <= len - 2; i++) {
+//                if (i < index) {
+//                    right = height[index];
+//                    for (int l = 0; l < i; l++) {
+//                        if (height[l] > left) {
+//                            left = height[l];
+//                        }
+//                    }
+//                }
+//
+//                if (i > index) {
+//                    left = height[index];
+//                    for (int r = i + 1; r < len; r++) {
+//                        if (height[r] > right) {
+//                            right = height[r];
+//                        }
+//                    }
+//                }
+//
+//                if (i != index) {
+//
+//                    sum += Math.max(Math.min(left, right) - height[i], 0);
+//                    left = 0;
+//                    right = 0;
+//                }
+//            }
+//            //优化2 动态规划 dp
+//            int[] maxleft = new int[len];
+//
+//            for (int i = 1; i < len - 1; i++) {
+//                if (maxleft[i - 1] < height[i - 1]) {
+//                    maxleft[i] = height[i - 1];
+//                } else {
+//                    maxleft[i] = maxleft[i - 1];
+//                }
+//
+//                if (height[len -  i  ] <maxRight[len - i]){
+//                    maxRight[len - 1 - i]  = maxRight[len - i];
+//                }else{
+//                    maxRight[len - 1 - i]  =height[len - i ];
+//                }
+//
+//            }
+//            for (int i = 1; i < len - 1; i++){
+//                sum += Math.max((Math.min(maxleft[i], maxRight[i]) - height[i]),0);
+//            }
+//            return sum;
+//        }
+            //优化3 dp + 单指针
+//            int[] maxRight = new int[len];
+//            int maxLeft = height[0];
+//            for (int i = len - 2; i >= 0; i--) {
+//                maxRight[i] = Math.max(maxRight[i + 1], height[i + 1]);
+//            }
+//
+//            for (int i = 1; i < len - 1; i++){
+//                if (maxLeft < height[i - 1]) {
+//                    maxLeft = height[i - 1];
+//                }
+//                sum += Math.max((Math.min(maxLeft, maxRight[i]) - height[i]), 0);
+//            }
+//
+//            return sum;
+//        }
 
-
-            for (int i = 1; i <= len - 2; i++) {
-                if (i < index){
-                    right = height[index];
-                    for (int l = 0; l < i; l++) {
-                        if (height[l] > left) {
-                            left = height[l];
-                        }
+            //优化4 dp + 双指针
+//            int ml = height[0],  left = 1, right = len - 2,mr = height[right + 1];
+//            while (left <= right) {
+//                if (ml < mr) {
+//                    if (ml > height[left]) {
+//                        sum += (ml - height[left]);
+//                    } else {
+//                        ml = height[left];
+//                    }
+//                    left ++;
+//                }else{
+//                    if (mr > height[right]) {
+//                        sum += (mr - height[right]);
+//                    } else {
+//                        mr = height[right];
+//                    }
+//                    right --;
+//                }
+//            }
+            //方法五 单调栈
+            Stack<Integer> stack = new Stack<>();
+            int cur = 0;
+            while (cur < len) {
+                while (!stack.empty() && height[cur] > height[stack.peek()]){
+                    int h = height[stack.peek()];
+                    stack.pop();
+                    if (stack.isEmpty()){
+                        break;
                     }
+                    int dis = cur - stack.peek() - 1;
+                    int min = Math.min(height[stack.peek()],height[cur]);
+                    sum   = sum + (dis* (min - h));
                 }
-
-                if (i > index){
-                    left = height[index];
-                    for (int r = i + 1; r < len; r++) {
-                        if (height[r] > right) {
-                            right = height[r];
-                        }
-                    }
-                }
-
-                if (i == index){
-                    break;
-                }
-
-
-                sum += Math.max(Math.min(left, right) - height[i], 0);
+                stack.push(cur);
+                cur ++;
             }
             return sum;
-        }
 
+        }
 
         private int getMaxIndex(int[] arr) {
             int len = arr.length;
@@ -123,6 +197,7 @@ public class TrappingRainWater {
             for (int i = 0; i < len; i++) {
                 if (arr[i] > max) {
                     res = i;
+                    max = arr[i];
                 }
             }
             return res;
