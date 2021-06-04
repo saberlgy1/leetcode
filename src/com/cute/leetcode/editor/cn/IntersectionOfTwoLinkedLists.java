ac0c657e1fc9d5f06ee43558fb67dd9655df5f50
刘光远 <liuguangyuan@neusoft.com>
2021-06-04 10:26:40 +0800

    feature(leetcode): 每日一题 相交节点

--- src/com/cute/leetcode/editor/cn/IntersectionOfTwoLinkedLists.java
+++ src/com/cute/leetcode/editor/cn/IntersectionOfTwoLinkedLists.java
@@ -1,60 +1,60 @@
-//编写一个程序，找到两个单链表相交的起始节点。 
+//编写一个程序，找到两个单链表相交的起始节点。
 //
-// 如下面的两个链表： 
+// 如下面的两个链表：
 //
-// 
 //
-// 在节点 c1 开始相交。 
 //
-// 
+// 在节点 c1 开始相交。
 //
-// 示例 1： 
 //
-// 
+//
+// 示例 1：
+//
+//
 //
 // 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, s
 //kipB = 3
 //输出：Reference of the node with value = 8
 //输入解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1
 //,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
-// 
 //
-// 
 //
-// 示例 2： 
 //
-// 
+//
+// 示例 2：
+//
+//
 //
 // 输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB =
 // 1
 //输出：Reference of the node with value = 2
 //输入解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4
 //]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
-// 
 //
-// 
 //
-// 示例 3： 
 //
-// 
+//
+// 示例 3：
+//
+//
 //
 // 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
 //输出：null
 //输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而
 // skipA 和 skipB 可以是任意值。
 //解释：这两个链表不相交，因此返回 null。
-// 
 //
-// 
 //
-// 注意： 
 //
-// 
-// 如果两个链表没有交点，返回 null. 
-// 在返回结果后，两个链表仍须保持原有的结构。 
-// 可假定整个链表结构中没有循环。 
-// 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。 
-// 
+//
+// 注意：
+//
+//
+// 如果两个链表没有交点，返回 null.
+// 在返回结果后，两个链表仍须保持原有的结构。
+// 可假定整个链表结构中没有循环。
+// 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+//
 // Related Topics 链表
 
 package com.cute.leetcode.editor.cn;
@@ -91,27 +91,40 @@ public class IntersectionOfTwoLinkedLists {
      * }
      */
     public class Solution {
-        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-//            //corner case
-//            if (headA == null || headB == null){
-//                return null;
-//            }
-//            //hash存储
-//            HashSet<ListNode> set = new HashSet<>();
-//            while (headA != null){
-//                set.add(headA);
-//                headA = headA.next;
-//            }
-//            int val = 0;
-//            while (headB!=null){
-//                if (set.contains(headB)){
-//                    break;
-//                }else{
-//                    headB = headB.next;
-//                }
-//            }
-//            return headB;
+        //思路一：hash存储 然后判断两条链表是否有相同节点元素
+        //时间复杂度O(2n) 空间复杂度O(n)
+       /* public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
             //corner case
+            if (headA == null || headB == null){
+                return null;
+            }
+            //hash存储
+            HashSet<ListNode> set = new HashSet<>();
+            while (headA != null){
+                set.add(headA);
+                headA = headA.next;
+            }
+            int val = 0;
+            while (headB!=null){
+                if (set.contains(headB)){
+                    break;
+                }else{
+                    headB = headB.next;
+                }
+            }
+            return headB;
+        }*/
+        //思路二：相交节点必然是两个链表头尾相连后相遇元素
+        //如下两个链表 相遇元素为10 节点
+        //1-3-5-7-9-10-11-12
+        //2-4-6-8-10-11-12
+        //头尾相连后
+        //1-3-5-7-9-10-11-12-2-4-6-8-10（回到10节点）
+        //2-4-6-8-10-11-12-1-3-5-7-9-10（回到10节点）
+        //相等即相遇
+        //时间复杂度O(n) 空间复杂度O(1)
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB){
+           // corner case
             ListNode left = headA;
 
             ListNode right = headB;
@@ -120,8 +133,8 @@ public class IntersectionOfTwoLinkedLists {
                 right = right == null? headA : right.next;
             }
             return left;
-
         }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
